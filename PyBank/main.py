7fbc10662eb7c83c29e7e41efe4259aa9d22b2ea
Riss <82292340+sissa81@users.noreply.github.com>
2021-05-31 18:39:23 -0700

    5/31/21 update for totals

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,10 @@
 import os
 import csv
 
+# starting count for profits and months
+total_profits = 0
+number_months = 0
+change_profits = 0
 # Path to collect budget data
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
@@ -9,19 +13,32 @@ with open(budget_csv) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    # Account for header row
     csvheader = next (csvfile)
-    # print(f"Header: {csvheader}")
+          
+    # Loop to total profits and months
+    for row in csvreader:
+        total_profits += int(row[1])
+        number_months += 1
 
-    value = len(list(csvreader))
-    print(f"Financial Analysis")
-    print(f"-----------------------------")
-    print(f"Total Months: {value}")
+        print(change_profits)
+    # Print Analysis
+    # print(f"Financial Analysis\n-----------------------------\nTotal Months: {number_months}\nTotal: {total_profits}")
+
+
 
     
-    # for row in csvreader:
-    # print(row)
+        
+# average = (total_profits) / (value)
+# print (average)
+
+    # Calc average
+            
+
         
 # with open(csvpath, "r") as file_handler:
 #     lines = file_handler.read()
 #     print(lines)
 #     print(type(lines))
+
+
