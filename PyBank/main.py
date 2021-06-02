2e54ddd984b5d2139706e61f751c150e542202f6
Riss <82292340+sissa81@users.noreply.github.com>
2021-06-01 18:54:46 -0700

    6/1/2021 Calc Max/Min Months

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,10 +2,11 @@
 import os
 import csv
 
-# Starting count for and months
-number_months = 0
+# Create starting point for total months
+Total_Months = 0
 
 # Create new list
+months_list = []
 profits_list = []
 change_profits_list = []
 
@@ -19,12 +20,13 @@ with open(budget_csv) as csvfile:
     # Account for header row
     csvheader = next (csvfile)
           
-    # Loop to append to profits list and to total profits / months 
+    # Loop to append to profits / months list and to total profits / months 
     for row in csvreader:
         profits_list.append(int(row[1]))
-        TotalProfits = "${:,.2f}".format(sum(profits_list))
-        number_months += 1
-
+        Total_Profits = "${:,.2f}".format(sum(profits_list))
+        months_list.append(row[0])
+        Total_Months += 1
+      
     # Loop to calculate change in profits
     for i in range(1, 86):
         change = profits_list[i] - profits_list[i-1]
@@ -35,26 +37,29 @@ with open(budget_csv) as csvfile:
     Number_Change = len(change_profits_list)
     Average_change = "${:,.2f}".format(Total_change/Number_Change)
     
-    # Find the max and min in the change list
+    # Find the max and min in the change list, one unformatted for index purposes
     max_change = "${:,.2f}".format(max(change_profits_list))   
     min_change = "${:,.2f}".format(min(change_profits_list))   
 
+    max_change_for_index = max(change_profits_list)
+    min_change_for_index = min(change_profits_list)
 
-
-
+    # Find the max and min position of max and min changes
+    max_index = change_profits_list.index(max_change_for_index)
+    min_index = change_profits_list.index(min_change_for_index)
     
-
-
-
-
-
+    # Find the max and min months
+    max_month = months_list[max_index]
+    min_month = months_list[min_index]
+    
+    
 
 
     # Print Analysis
 print("Financial Analysis")
 print("-----------------------------")
-print(f"Total Months: {number_months}")
-print(f"Total: {TotalProfits}")
+print(f"Total Months: {Total_Months}")
+print(f"Total: {Total_Profits}")
 print(f"Average Change: {Average_change}")
-print(f"Greatest Increase in Profits: {max_change}")
-print(f"Greatest Increase in Profits: {min_change}")
+print(f"Greatest Increase in Profits: {max_month} {max_change}")
+print(f"Greatest Increase in Profits: {min_month} {min_change}")
