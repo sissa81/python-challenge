cfcb34c404846bdd38dd84abdd480155db7b42d0
Riss <82292340+sissa81@users.noreply.github.com>
2021-06-01 17:49:21 -0700

    6/1/21 Calc Change In Profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,10 +2,14 @@
 import os
 import csv
 
-# starting count for profits and months
+# Starting count for profits and months
 total_profits = 0
 number_months = 0
-change_profits = 0
+
+# Create new list
+profits_list = []
+change_profits_list = []
+
 # Path to collect budget data
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
@@ -16,29 +20,34 @@ with open(budget_csv) as csvfile:
     # Account for header row
     csvheader = next (csvfile)
           
-    # Loop to total profits and months
+    # Loop to total profits / months 
     for row in csvreader:
+        profits_list.append(int(row[1]))
         total_profits += int(row[1])
         number_months += 1
 
-        print(change_profits)
-    # Print Analysis
-    # print(f"Financial Analysis\n-----------------------------\nTotal Months: {number_months}\nTotal: {total_profits}")
+    # Loop to calculate change in profits
+    for i in range(1, 86):
+        change = profits_list[i] - profits_list[i-1]
+        change_profits_list.append(change)
 
-
+    # Variables to calcuate average change in profits          
+    Total_change = sum(change_profits_list)
+    Number_Change = len(change_profits_list)
+    Average_change = "${:,.2f}".format(Total_change/Number_Change)
+    
+    
+    print(Average_change)
 
     
-        
-# average = (total_profits) / (value)
-# print (average)
 
-    # Calc average
-            
 
-        
-# with open(csvpath, "r") as file_handler:
-#     lines = file_handler.read()
-#     print(lines)
-#     print(type(lines))
+
+
+
+
+
+    # Print Analysis
+    # print(f"Financial Analysis\n-----------------------------\nTotal Months: {number_months}\nTotal: ${total_profits}")
 
 
