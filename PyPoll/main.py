9ad213ae22c7efe252a5583f230d9c7bd3cc8ae0
Riss <82292340+sissa81@users.noreply.github.com>
2021-06-02 21:36:31 -0700

    6/2/2021 Final Without Candidates as Variables

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -27,26 +27,18 @@ with open(votes_csv) as csvfile:
     for row in csvreader:
         candidate_list.append(row[2])
         Total_Votes += 1
-
-    # Format total votes with commas
-    Total_Votes_formatted = "{:,.0f}".format(Total_Votes)
            
     # Create a sorted list of unique candidate names
     candidate_list_unique = sorted(list(set(candidate_list)))
     # print(candidate_list_unique)
     # ['Correy', 'Khan', 'Li', "O'Tooley"]
 
-    # Count votes per candidate, one unformatted to work for percentage calc and one formatted to show commas    
+    # Count votes per candidate    
     Correy_Count = candidate_list.count("Correy")
     Khan_Count = candidate_list.count("Khan")
     Li_Count = candidate_list.count("Li")
     OTooley_Count = candidate_list.count("O'Tooley")
-
-    Correy_Count_formatted = "{:,.0f}".format(candidate_list.count("Correy"))
-    Khan_Count_formatted = "{:,.0f}".format(candidate_list.count("Khan"))
-    Li_Count_formatted = "{:,.0f}".format(candidate_list.count("Li"))
-    OTooley_Count_formatted = "{:,.0f}".format(candidate_list.count("O'Tooley"))
-    
+      
     # Calculate percentages of votes
     Correy_percent = "{:.3%}".format(Correy_Count/Total_Votes)
     Khan_percent = "{:.3%}".format(Khan_Count/Total_Votes)    
@@ -70,32 +62,32 @@ with open(votes_csv) as csvfile:
     
    
     # Print Analysis in Terminal
-    # print("Election Results")
-    # print("-----------------------------")
-    # print(f"Total Votes: {Total_Votes_formatted}")
-    # print("-----------------------------")
-    # print(f"{candidate_list_unique[0]}: {candidate_percent_list[0]} ({candidate_vote_list[0]})")
-    # print(f"{candidate_list_unique[1]}: {candidate_percent_list[1]} ({candidate_vote_list[1]})")
-    # print(f"{candidate_list_unique[2]}: {candidate_percent_list[2]} ({candidate_vote_list[2]})")
-    # print(f"{candidate_list_unique[3]}: {candidate_percent_list[3]} ({candidate_vote_list[3]})")
-    # print("-----------------------------")
-    # print(f"Winner: {most_votes_candidate}")
-    # print("-----------------------------")
+    print("Election Results")
+    print("-----------------------------")
+    print(f"Total Votes: {Total_Votes}")
+    print("-----------------------------")
+    print(f"{candidate_list_unique[0]}: {candidate_percent_list[0]} ({candidate_vote_list[0]})")
+    print(f"{candidate_list_unique[1]}: {candidate_percent_list[1]} ({candidate_vote_list[1]})")
+    print(f"{candidate_list_unique[2]}: {candidate_percent_list[2]} ({candidate_vote_list[2]})")
+    print(f"{candidate_list_unique[3]}: {candidate_percent_list[3]} ({candidate_vote_list[3]})")
+    print("-----------------------------")
+    print(f"Winner: {most_votes_candidate}")
+    print("-----------------------------")
 
     # Print Analysis to Text File    
-    # Text_File = open(f"PyPoll.txt","w")
-    # Text_File.write(f"Election Results\n")
-    # Text_File.write(f"-----------------------------\n")
-    # Text_File.write(f"Total Votes: {Total_Votes}\n")
-    # Text_File.write(f"-----------------------------\n")
-    # Text_File.write(f"{candidate_list_unique[0]}: {candidate_percent_list[0]} ({candidate_vote_list[0]})\n")
-    # Text_File.write(f"{candidate_list_unique[1]}: {candidate_percent_list[1]} ({candidate_vote_list[1]})\n")
-    # Text_File.write(f"{candidate_list_unique[2]}: {candidate_percent_list[2]} ({candidate_vote_list[2]})\n")
-    # Text_File.write(f"{candidate_list_unique[3]}: {candidate_percent_list[3]} ({candidate_vote_list[3]})\n")
-    # Text_File.write(f"-----------------------------\n")
-    # Text_File.write(f"Winner: {most_votes_candidate}\n")
-    # Text_File.write(f"-----------------------------")    
-    # Text_File.close()
+    Text_File = open(f"PyPoll.txt","w")
+    Text_File.write(f"Election Results\n")
+    Text_File.write(f"-----------------------------\n")
+    Text_File.write(f"Total Votes: {Total_Votes}\n")
+    Text_File.write(f"-----------------------------\n")
+    Text_File.write(f"{candidate_list_unique[0]}: {candidate_percent_list[0]} ({candidate_vote_list[0]})\n")
+    Text_File.write(f"{candidate_list_unique[1]}: {candidate_percent_list[1]} ({candidate_vote_list[1]})\n")
+    Text_File.write(f"{candidate_list_unique[2]}: {candidate_percent_list[2]} ({candidate_vote_list[2]})\n")
+    Text_File.write(f"{candidate_list_unique[3]}: {candidate_percent_list[3]} ({candidate_vote_list[3]})\n")
+    Text_File.write(f"-----------------------------\n")
+    Text_File.write(f"Winner: {most_votes_candidate}\n")
+    Text_File.write(f"-----------------------------")    
+    Text_File.close()
 
     
 
